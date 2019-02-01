fed7f0d2a47294e6a4222756d3d21b8f1d595c24
Stuart R Jefferys <srj@unc.edu>
2019-02-01 17:43:22 +0000

    DOC: Add a package documentation page.

---
+++ R/JefferysRUtils.R
@@ -0,0 +1,13 @@
+#' JefferysRUtils: My bag of tricks for R coding
+#'
+#' A collection of utilities that I use in R code. Each is too small to
+#' warrant their own package, they are relatively unrelated, and depending on
+#' them all as separate utility packages is too many dependencies for now. May
+#' split some out later, so be warned.
+
+#' These are useful for me. Your milage may vary. Some are unfinished
+#' and possibly broken
+#'
+#' @docType package
+#' @name JefferysRUtils
+NULL
